306be645e19494fd58381a02b533a33f630e4a0f
SeanUmpad <sean_umpad@dlsu.edu.ph>
2023-11-23 17:59:50 +0800

    Updated player stats page
    - now reflects the updated schema

--- app/src/main/java/com/mobdeve/machineproject/PlayerStatsRecycler/PlayerStatsViewHolder.kt
+++ app/src/main/java/com/mobdeve/machineproject/PlayerStatsRecycler/PlayerStatsViewHolder.kt
@@ -1,7 +1,6 @@
 package com.mobdeve.machineproject.PlayerStatsRecycler
 
 import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
@@ -12,15 +11,18 @@ import com.mobdeve.machineproject.R
 class PlayerStatsViewHolder(itemView: View): ViewHolder(itemView) {
     private val playerStatName: TextView = itemView.findViewById(R.id.playerStat_name)
     private val playerStatImg: ImageView = itemView.findViewById(R.id.playerStat_img)
-    private val playerStatTimesPlayed: TextView = itemView.findViewById(R.id.playerStat_timesPlayed)
-    private val playerStatTimesWon: TextView = itemView.findViewById(R.id.playerStat_timesWon)
+    private val survivorGames: TextView = itemView.findViewById(R.id.survivorGames_numbers)
+    private val survivorWins: TextView = itemView.findViewById(R.id.survivorWins_numbers)
+    private val viralGames: TextView = itemView.findViewById(R.id.viralGames_numbers)
+    private val viralInfections: TextView = itemView.findViewById(R.id.viralInfection_numbers)
     val deleteButton: ImageButton = itemView.findViewById(R.id.playerStatDelete)
 
     fun bind(playerStat: Player) {
         playerStatName.text = playerStat.name
         playerStatImg.setImageResource(playerStat.playerImg)
-        //Placeholder for now until stat view changes
-        playerStatTimesPlayed.text = playerStat.numSurvivorPlayed.toString()
-        playerStatTimesWon.text = playerStat.numSurvivorWins.toString()
+        survivorGames.text = playerStat.numSurvivorPlayed.toString()
+        survivorWins.text = playerStat.numSurvivorWins.toString()
+        viralGames.text = playerStat.numViralPlayed.toString()
+        viralInfections.text = playerStat.numViralInfections.toString()
     }
 }
